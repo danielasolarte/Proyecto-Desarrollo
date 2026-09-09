3da65d9ccf8e236979b2d763d1259f3481823079
Camilafajardo22 <c.fajardos@uniandes.edu.co>
2026-09-09 07:22:26 -0500

    repository y service para quiz attempts

---
+++ migrations/000003_create_quizzes_progress_badges_tables.down.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS badge_issuances;
+DROP TABLE IF EXISTS badges;
+
+DROP TABLE IF EXISTS course_progress;
+DROP TABLE IF EXISTS progress_events;
+DROP TABLE IF EXISTS resource_progress;
+
+DROP TABLE IF EXISTS attempt_answers;
+DROP TABLE IF EXISTS attempts;
+DROP TABLE IF EXISTS question_options;
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS quizzes;
